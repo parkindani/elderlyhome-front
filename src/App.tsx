25fd7f9a3d7ea24e5f26c69617a75381aa6d39ac
parkindani <parkindani@gmail.com>
2021-04-21 18:20:22 +0900

    styled working

--- src/App.tsx
+++ src/App.tsx
@@ -1,110 +1,131 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import useSupercluster from "use-supercluster";
-import axios from 'axios';
-import useSWR from 'swr'
+import axios from "axios";
+import useSWR from "swr";
+import styled from "styled-components";
 
-const fetcher = (url: string) => axios.get(url).then(res => res.data.OldPersonRecuperationFacility[1].row);
+const fetcher = (url: string) =>
+  axios.get(url).then((res) => res.data.OldPersonRecuperationFacility[1].row);
 
 const Marker = ({ children }: any) => children;
 
 const ElderlyMap = () => {
-
-  console.log('api', process.env.REACT_APP_API_KEY);
-
   const [bounds, setBounds] = useState<any[]>([]);
   const [zoom, setZoom] = useState(10);
   const url = "/data/data.json";
-  const {data, error} = useSWR(url, fetcher);
-  const homes = data && ! error ? data : [];
-  const points = homes.map((home: { LICENSG_DE: string; BIZPLC_NM: string; REFINE_WGS84_LOGT: string; REFINE_WGS84_LAT: string; }) => ({
-    type: "Feature",
-    properties: { cluster: false, homeId: home.LICENSG_DE, homeName: home.BIZPLC_NM },
-    geometry: {
-      type: "Point",
-      coordinates: [
-        parseFloat(home.REFINE_WGS84_LOGT),
-        parseFloat(home.REFINE_WGS84_LAT)
-      ]
-    }
-  }));
-
+  const { data, error } = useSWR(url, fetcher);
+  const homes = data && !error ? data : [];
+  const points = homes.map(
+    (home: {
+      LICENSG_DE: string;
+      BIZPLC_NM: string;
+      REFINE_WGS84_LOGT: string;
+      REFINE_WGS84_LAT: string;
+    }) => ({
+      type: "Feature",
+      properties: {
+        cluster: false,
+        homeId: home.LICENSG_DE,
+        homeName: home.BIZPLC_NM,
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          parseFloat(home.REFINE_WGS84_LOGT),
+          parseFloat(home.REFINE_WGS84_LAT),
+        ],
+      },
+    })
+  );
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const {clusters, supercluster} = useSupercluster({
+  const { clusters, supercluster } = useSupercluster({
     points,
     bounds,
     zoom,
-    options: {radius: 75, maxZoom: 20}
-  })
+    options: { radius: 75, maxZoom: 20 },
+  });
 
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
+    <Style>
+          <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
-
-      
         bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY! }}
         defaultCenter={{ lat: 37.5326, lng: 127.024612 }}
         defaultZoom={11}
-        onChange={({zoom, bounds}) => {
+        onChange={({ zoom, bounds }) => {
           setZoom(zoom);
           setBounds([
             bounds.nw.lng,
             bounds.se.lat,
             bounds.se.lng,
-            bounds.nw.lat
-          ])
+            bounds.nw.lat,
+          ]);
         }}
       >
-
-        {
-          clusters.map((cluster, index) => {
+        
+          {clusters.map((cluster, index) => {
             const [longitude, latitude] = cluster.geometry.coordinates;
             const {
               cluster: isCluster,
               point_count: pointCount,
-              homeName
+              homeName,
             } = cluster.properties;
 
             if (isCluster) {
               return (
-                <Marker
-                key={`cluster-${index}`}
-                lat={latitude}
-                lng={longitude}
-              >
-                <div
-                  className="cluster-marker"
-                  style={{
-                    width: `${10 + (pointCount / points.length) * 20}px`,
-                    height: `${10 + (pointCount / points.length) * 20}px`
-                  }}
-                  onClick={() => {}}
-                >
-                  {pointCount}
-                </div>
-              </Marker>
+                <Marker key={`cluster-${index}`} lat={latitude} lng={longitude}>
+                  <div
+                    className="cluster-marker"
+                    style={{
+                      width: `${10 + (pointCount / points.length) * 20}px`,
+                      height: `${10 + (pointCount / points.length) * 20}px`,
+                    }}
+                    onClick={() => {}}
+                  >
+                    {pointCount}
+                  </div>
+                </Marker>
               );
             }
 
             return (
-              <Marker
-                key={`crime-${index}`}
-                lat={latitude}
-                lng={longitude}
-              >
-                <button className="crime-marker">
+              <Marker key={`home-${index}`} lat={latitude} lng={longitude}>
+                <button className="home-marker">
                   <p>{homeName}</p>
                 </button>
               </Marker>
             );
-
-          })
-        }
-        
+          })}
       </GoogleMapReact>
     </div>
+
+    </Style>
+
   );
 };
+
+const Style = styled.div`
+.cluster-marker {
+  color: #fff;
+  background: #1978c8;
+  border-radius: 50%;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.home-marker {
+  background: white;
+  border: green;
+  min-width: 120px;
+}
+
+.home-marker img {
+  width: 25px;
+}
+`;
 
 export default ElderlyMap;
