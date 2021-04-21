a8e921f6b1546e42f40cf99cdb6182f0d41bc006
parkindani <parkindani@gmail.com>
2021-04-21 17:20:44 +0900

    show markers

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,40 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import GoogleMapReact from "google-map-react";
-import getMarkers from "./api/marker";
+import useSupercluster from "use-supercluster";
+import axios from 'axios';
+import useSWR from 'swr'
 
-type AnyReactComponentProps = {
-  text: any;
-  lat: any;
-  lng: any;
-};
-const Marker = ({ text }: AnyReactComponentProps) => (
-  <div>{text}</div>
-);
+
+const fetcher = (url: string) => axios.get(url).then(res => res.data.OldPersonRecuperationFacility[1].row);
+
+const Marker = ({ children }: any) => children;
 
 const ElderlyMap = () => {
 
-  const [markers, setMarkers] = useState<any[]>([]);
-
-  useEffect(()=> {
-    let complete = false;
-    async function get() {
-      const result = await getMarkers();
-      if (!complete) setMarkers(result);      
+  const [bounds, setBounds] = useState<any[]>([]);
+  const [zoom, setZoom] = useState(10);
+  const url = "/data/data.json";
+  const {data, error} = useSWR(url, fetcher);
+  const homes = data && ! error ? data : [];
+  const points = homes.map((home: { LICENSG_DE: string; BIZPLC_NM: string; REFINE_WGS84_LOGT: string; REFINE_WGS84_LAT: string; }) => ({
+    type: "Feature",
+    properties: { cluster: false, homeId: home.LICENSG_DE, homeName: home.BIZPLC_NM },
+    geometry: {
+      type: "Point",
+      coordinates: [
+        parseFloat(home.REFINE_WGS84_LOGT),
+        parseFloat(home.REFINE_WGS84_LAT)
+      ]
     }
-    get();
-    return () => {
-      complete = true;
-    };
-  },[]);
+  }));
+
+
+  const {clusters, supercluster} = useSupercluster({
+    points,
+    bounds,
+    zoom,
+    options: {radius: 75, maxZoom: 20}
+  })
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
@@ -33,11 +42,61 @@ const ElderlyMap = () => {
         bootstrapURLKeys={{ key: "AIzaSyCAKQJBl3rgofeiyxOVqpcOpNw_PZXYCTY" }}
         defaultCenter={{ lat: 37.5326, lng: 127.024612 }}
         defaultZoom={11}
+        onChange={({zoom, bounds}) => {
+          setZoom(zoom);
+          setBounds([
+            bounds.nw.lng,
+            bounds.se.lat,
+            bounds.se.lng,
+            bounds.nw.lat
+          ])
+        }}
       >
 
-        { markers !== [] ? markers.map((e,i) => {
-          return <Marker lat={e.REFINE_WGS84_LAT} lng={e.REFINE_WGS84_LOGT} text={e.BIZPLC_NM} key = {i}/>
-        }) : ''}
+        {
+          clusters.map((cluster, index) => {
+            const [longitude, latitude] = cluster.geometry.coordinates;
+            const {
+              cluster: isCluster,
+              point_count: pointCount,
+              homeName
+            } = cluster.properties;
+
+            if (isCluster) {
+              return (
+                <Marker
+                key={`cluster-${index}`}
+                lat={latitude}
+                lng={longitude}
+              >
+                <div
+                  className="cluster-marker"
+                  style={{
+                    width: `${10 + (pointCount / points.length) * 20}px`,
+                    height: `${10 + (pointCount / points.length) * 20}px`
+                  }}
+                  onClick={() => {}}
+                >
+                  {pointCount}
+                </div>
+              </Marker>
+              );
+            }
+
+            return (
+              <Marker
+                key={`crime-${index}`}
+                lat={latitude}
+                lng={longitude}
+              >
+                <button className="crime-marker">
+                  <p>{homeName}</p>
+                </button>
+              </Marker>
+            );
+
+          })
+        }
         
       </GoogleMapReact>
     </div>
